791f23acb1d378d087a8a8fa5052de5db2168386
Alex Useche <alex.useche@nvisium.com>
2020-05-23 18:21:34 -0600

    fixed section type and print format

--- src/binary.rs
+++ src/binary.rs
@@ -1,5 +1,9 @@
 use goblin::elf::Elf;
 use goblin::mach::MachO;
+use goblin::elf::section_header::sht_to_str;
+use goblin::mach::constants as mach_constants;
+
+use std::str;
 
 use serde::{Deserialize, Serialize};
 
@@ -62,7 +66,7 @@ pub struct Symbol {
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Section {
     pub name: String,
-    pub sectype: u32, //TODO: Parse hex value and match to a SecType
+    pub sectype: String, //TODO: Parse hex value and match to a SecType
     pub vma: u64,
     pub size: u64,
 }
@@ -80,20 +84,23 @@ pub struct Binary {
 
 impl Binary {
     pub fn print_symbols(&self) {
-        println!("{:>6} {:>42} {:>5}", "Name", "Address", "Type");
+        println!("{:10} {:>42} {:>5}", "VAddress", "Type", "Name");
+        println!("{:-<1$}", "", 60);
         for sym in &self.symbols {
             println!(
-                "  {:40} {:#5x?}    {:>70?}",
-                sym.name, sym.addr, sym.symboltype
+                "{:#010x?} {:#70?} {:10}",
+                sym.addr, sym.symboltype, sym.name,
             );
         }
+        println!("Number of Symbols: {}", &self.symbols.len());
     }
 
     pub fn print_sections(&self) {
-        println!("{} {:>5} {:>20} {:>29}", "Address", "Size", "Name", "Type");
+        println!("{} {:>11} {:<20} {:10}", "VAddress", "Size", "Name", "Type");
+        println!("{:-<1$}", "", 60);
         for sec in &self.sections {
             println!(
-                "{:#x?} {:<20?} {:<20} {:10?}",
+                "{:#010x?} {:#10x?} {:#20} {:10}",
                 sec.vma, sec.size, sec.name, sec.sectype
             );
         }
@@ -120,7 +127,7 @@ impl BinSections for &Elf<'_> {
                     .expect("Error getting section")
                     .expect("Error obtaining section name")
                     .to_string(), // TODO: get from sec.sh_name,
-                sectype: sec.sh_type, // TODO: Parse as SecType
+                sectype: sht_to_str(sec.sh_type).to_string(), // TODO: Parse as SecType
                 vma: sec.sh_addr,
                 size: sec.sh_size,
             });
@@ -146,9 +153,10 @@ impl BinSections for &MachO<'_> {
                     Ok(name) => name.to_string(),
                     Err(_err) => "Invalid Name".to_string(),
                 };
+                let sec_type = sec.0.flags & mach_constants::SECTION_TYPE;
                 result.push(Section {
                     name,
-                    sectype: sec.0.flags, //TODO: Need to find a way to get type of MachO section
+                    sectype: s_type_to_str(sec_type).to_string(), //TODO: Need to find a way to get type of MachO section
                     vma: sec.0.addr,
                     size: sec.0.size,
                 });
@@ -216,5 +224,15 @@ impl BinSymbols for &MachO<'_> {
             });
         }
         result
+    }
+}
+
+fn s_type_to_str(sec:u32) -> &'static str {
+    match sec{
+        mach_constants::S_REGULAR => "S_REGULAR",
+        mach_constants::S_ZEROFILL => "S_ZEROFILL",
+        mach_constants::S_NON_LAZY_SYMBOL_POINTERS => "S_NON_LAZY_SYMBOL_POINTERS",
+        mach_constants::S_SYMBOL_STUBS => "S_SYMBOL_STUBS",
+        _ => "UNKNOWN"
     }
 }
